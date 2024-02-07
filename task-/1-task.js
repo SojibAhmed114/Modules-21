30198c2f23c600d9f0ca0562415316f136b6f6ad
SojihAhmed114 <sojib.ahmed7620@gmail.com>
2024-02-07 13:47:39 +0600

    baki task gula pari nai

---
+++ task-/1-task.js
@@ -0,0 +1,8 @@
+// Task-1:
+// Write a function to convert temperature from Celsius to Fahrenheit.
+
+function celsiusToF(celsius) {
+    const fahrenheit = (celsius * 9 / 5) + 32;
+    return fahrenheit;
+}
+console.log(celsiusToF(62)); 
